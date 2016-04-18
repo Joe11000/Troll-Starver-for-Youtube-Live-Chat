8f3f387a5a5fcda7af1d2db84be66d94b4a0db55
Joe Noonan <joenoonan27@gmail.com>
2016-04-17 21:43:25 -0500

    things are working again

--- background.js
+++ background.js
@@ -1,13 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
+  chrome.tabs.insertCSS(tab.id, { "file": "css/troll_starver.css" });
 
-  chrome.tabs.insertCSS(tab.id, {
-      "file": "css/troll_starver.css"
-  }, function () { // Execute your code
-  });
-
-  chrome.tabs.executeScript(tab.id, {
-      "file": "js/troll_starver.js"
-  }, function () { // Execute your code
-  });
-
+  chrome.tabs.executeScript(tab.id, { "file": "js/troll_starver.js" });
 });
--- js/troll_starver_es6.js
+++ js/troll_starver_es6.js
@@ -1,32 +1,41 @@
-var troll_img_src = chrome.extension.getURL("images/trollx60.png");
-var remove_name_src = chrome.extension.getURL("images/remove-name.png");
-// document.getElementById("someImage").src = imgURL;
 
 
-$('#live-comments-controls').append(`
-
-  <div id='troll-extension-wrapper'>
-    <div id='troll-image-wrapper' droppable='true' ondragover="event.preventDefault();" ondrop="users_id_url = event.dataTransfer.getData('text');
-                          debugger;
-                          users_id = users_id_url.match(/channel\/(.+)$/)[1];
-                          selector_str = 'a[data-ytid=' + users_id + ']';
-                          name = $(selector_str).html();
-                          console.warn(name);
-                          "
-                          >
+  var troll_img_src = chrome.extension.getURL("images/trollx60.png");
+  var remove_name_src = chrome.extension.getURL("images/remove-name.png");
+  // document.getElementById("someImage").src = imgURL;
 
 
-      <img alt='Drag names of trolls to Ignore' src=${troll_img_src}>
+  $('#live-comments-controls').append(`
+
+    <div id='troll-extension-wrapper'>
+      <div id='troll-image-wrapper' droppable='true' ondragover="event.preventDefault();" ondrop="users_id_url = event.dataTransfer.getData('text');
+                            debugger;
+                            users_id = users_id_url.match(/channel\/(.+)$/)[1];
+                            selector_str = 'a[data-ytid=' + users_id + ']';
+                            name = $(selector_str).html();
+                            console.warn(name);
+                            "
+                            >
+
+
+        <img alt='Drag names of trolls to Ignore' src=${troll_img_src}>
+      </div>
+
+      <ul id='troll-names-wrapper'>
+        <li class='troll'>
+          <img class='remove-name' src=${remove_name_src} onclick="console.warn('remove troll from list')"></img>
+          <label data-id='name'>Joser Noonski</label>
+          <span data-id='comment-counter'>0</span>
+        </li>
+      </ul>
+
+      <a id="clear-all-comments" href='#'>clear chat</a>
     </div>
+  `)
 
-    <ul id='troll-names-wrapper'>
-      <li class='troll'>
-        <img class='remove-name' src=${remove_name_src} onclick="console.warn('remove troll from list')"></img>
-        <label data-id='name'>Joser Noonski</label>
-        <span data-id='comment-counter'>0</span>
-      </li>
-    </ul>
 
-    <a href='#' onclick="event.preventDefault(); document.querySelector('#all-comments').innerHTML = ''">clear chat</a>
-  </div>
-`)
+  // unabtrusive js
+  $('#clear-all-comments').on('click', function(){
+    event.preventDefault();
+    $('all-comments').html('');
+  });
