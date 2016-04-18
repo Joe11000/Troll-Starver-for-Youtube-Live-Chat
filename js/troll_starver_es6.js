a5eb19785c17f5855c27a00755bf784c31ad87f7
Joe Noonan <joenoonan27@gmail.com>
2016-04-17 22:55:39 -0500

    can get trolls name by dragging it onto troll icon

--- js/troll_starver_es6.js
+++ js/troll_starver_es6.js
@@ -1,4 +1,4 @@
-
+if($('#troll-extension-wrapper').length == 0) {
 
   var troll_img_src = chrome.extension.getURL("images/trollx60.png");
   var remove_name_src = chrome.extension.getURL("images/remove-name.png");
@@ -8,14 +8,7 @@
   $('#live-comments-controls').append(`
 
     <div id='troll-extension-wrapper'>
-      <div id='troll-image-wrapper' droppable='true' ondragover="event.preventDefault();" ondrop="users_id_url = event.dataTransfer.getData('text');
-                            debugger;
-                            users_id = users_id_url.match(/channel\/(.+)$/)[1];
-                            selector_str = 'a[data-ytid=' + users_id + ']';
-                            name = $(selector_str).html();
-                            console.warn(name);
-                            "
-                            >
+      <div id='troll-image-wrapper' droppable='true' ondragover="event.preventDefault();">
 
 
         <img alt='Drag names of trolls to Ignore' src=${troll_img_src}>
@@ -39,3 +32,12 @@
     event.preventDefault();
     $('all-comments').html('');
   });
+
+  $('#troll-image-wrapper').on('drop', function(){
+      users_id_url = event.dataTransfer.getData('text');
+      users_id = users_id_url.match(/channel\/(.+)$/)[1];
+      selector_str = 'a[data-ytid=' + users_id + ']';
+      name = $(selector_str).html();
+      console.warn(name);
+  });
+}
